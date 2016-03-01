a25e9738bb571c06a4fdb0edbd48ad969cd7fd00
Prateek Shrivastava <Pshrivastava@dminc.com>
2016-03-01 11:05:54 -0500

    For resource and time management, I have created the functions which will store cached values and provide them as needed
    This would help resource intensive tasks to run faster.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow users to calculate inverse of a matrix. If it is 
+## already calculated, then it returns the cached value of the inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix which has the capability to cache 
+## its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s<- NULL
+        set<- function(y){
+                x<<-y
+                s<<-NULL
+        }
+        get<- function() x
+        setinverse<- function(solve) s <<- solve
+        getinverse<- function() s
+        list(set=set, get= get, 
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve checks if the inverse has already been calculated, if yes, it 
+## returns cached value, else it calculates and return inverse of the special 
+## vector
 
 cacheSolve <- function(x, ...) {
+        s <- x$getinverse()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setinverse(s)
+        s
         ## Return a matrix that is the inverse of 'x'
 }
